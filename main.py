15c5d465d520d4b294779963e9ff1ddd0e81f7bd
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-07 12:12:11 -0700

    added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from initial_text import show_initial_text
+
+def main():
+
+    # inital welcome user interface
+    show_initial_text()
+
+    # read in excel data and create packages and store in hash table
+
+    # place packages in trucks
+
+    # show user interface
+
+
+if __name__ == '__main__':
+    main()
