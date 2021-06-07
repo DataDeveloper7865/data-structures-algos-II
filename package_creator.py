22133300b831abbfcfb123d79ac3b7dfaca48711
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-07 12:38:54 -0700

    added package creator

---
+++ package_creator.py
@@ -0,0 +1,21 @@
+import csv
+from package import Package
+
+
+def create_packages():
+    with open('./data/input_data.csv') as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
+
+        for row in read_csv:
+            new_package = Package(
+                package_id = row[0], 
+                address = row[1], 
+                city = row[2], 
+                state = row[3],
+                zip_code = row[4], 
+                deadline = row[5],
+                kilos = row[6],
+                special_notes = row[7]
+            )
+
+            print("Packages in Hub: ", new_package)
