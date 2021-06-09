4695da60f80eb7fe17bc04842964307ffb9f2471
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-09 12:37:54 -0700

    added exit from program

--- helpers/get_all_package_info.py
+++ helpers/get_all_package_info.py
@@ -4,4 +4,10 @@
 def get_all_package_info():
 
     print("hello from get all package info")
+    
+
+    # do some stuff here
+    exit()
+
+ 
    
--- helpers/get_single_package_info.py
+++ helpers/get_single_package_info.py
@@ -3,4 +3,11 @@
 def get_single_package_info():
 
     print("hello world from single package info")
-    
+
+    print("The program will exit now")
+
+
+    # do some stuff here
+
+
+
