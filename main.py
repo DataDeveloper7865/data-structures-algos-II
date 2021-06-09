81cbb6781915606adeb7aec55c92b3d1c90742a0
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-09 12:03:40 -0700

    added show packages

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ from helpers.show_user_interface_cli import show_user_interface
 from helpers.package_creator import create_packages
 from helpers.show_initial_text import show_initial_text
 from models.truck import Truck
+from models.hash_table import HashMap
 
 def main():
 
     # inital welcome user interface
     show_initial_text()
 
-    # read in excel data and create packages and store in hash table
-    create_packages()
-
-    # place packages in trucks
-
-
+    # instantiate trucks, accepts truck number and departure time
     truck_1 = Truck(1, "noon")
     truck_2 = Truck(2, "noon")
     truck_3 = Truck(3, "noon")
 
-    print(truck_1)
-    print(truck_2)
-    print(truck_3)
+    # instantiate hash map to hold values for fast lookup
+    package_holder = HashMap()
+
+
+    # 1. read in excel data 
+    # 2. create packages 
+    # 3. store in hash table
+    # 4. place packages into trucks
+    create_packages(package_holder, truck_1, truck_2, truck_3)
+
+
+    #print packages in trucks
+    truck_1.show_packages()
+    truck_2.show_packages()
+    truck_3.show_packages()
+
+    # place packages in trucks
 
     # show user interface
     show_user_interface()
