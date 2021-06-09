76c8524e71632180917846e2856b92552739a0d1
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-09 12:42:45 -0700

    added shortest path to trucks

--- models/truck.py
+++ models/truck.py
@@ -19,30 +19,32 @@ class Truck:
             print(package)
 
 
-    def determine_shortest_path(self, current_location):
-        if not len(self.package_list):
-            return self.package_list
+    # def determine_shortest_path(self, current_location):
+    #     if not len(self.package_list):
+    #         return self.package_list
 
-        smallest_distance = 50.0
-        location = 0
-        total_distance = 0
+    #     smallest_distance = 50.0
+    #     location = 0
+    #     total_distance = 0
 
 
         # 1. iterate through package list
         # 2. if the distance from the current location to the next address is smaller,
         #    set the smallest distance equal to the current distance being compared
-        for package in self.package_list:
-            address_to_compare = package.address
-            if get_current_distance(current_location, address_to_compare) <= smallest_distance:
-                smallest_distance = get_current_distance(
-                    current_location, address_to_compare)
-                location = address_to_compare
+        # for package in self.package_list:
+        #     address_to_compare = package.address
+        #     if get_current_distance(current_location, address_to_compare) <= smallest_distance:
+        #         smallest_distance = get_current_distance(
+        #             current_location, address_to_compare)
+        #         location = address_to_compare
 
-        for package in self.package_list:
-            if get_current_distance(current_location, package.address) == smallest_distance:
-                self.ordered_package_list.append(package)
-                self.package_list.pop(package)
-                current_location = location
-                total_distance += smallest_distance
-                determine_shortest_path(self.package_list, current_location)
+
+        # TODO: Implement function
+        # for package in self.package_list:
+        #     if get_current_distance(current_location, package.address) == smallest_distance:
+        #         self.ordered_package_list.append(package)
+        #         self.package_list.pop(package)
+        #         current_location = location
+        #         total_distance += smallest_distance
+        #         determine_shortest_path(self.package_list, current_location)
 
