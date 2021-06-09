d26f48c2bb7f7e06a4c2edea5a72b78d13fb6253
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-06-09 12:05:30 -0700

    added packages creator method

--- helpers/package_creator.py
+++ helpers/package_creator.py
@@ -2,13 +2,14 @@ import csv
 from models.package import Package
 
 
-def create_packages():
+def create_packages(package_list, truck_1, truck_2, truck_3):
+
     with open('./data/input_data.csv') as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
 
         for row in read_csv:
             new_package = Package(
-                package_id = row[0], 
+                package_id = row[0],
                 address = row[1], 
                 city = row[2], 
                 state = row[3],
@@ -18,4 +19,25 @@ def create_packages():
                 special_notes = row[7]
             )
 
-            print("Packages in Hub: ", new_package)
+            # TODO: fix package_list
+            # insert package into hash table for fast lookup
+            # package_list.insert_value_into_hash_table(new_package.id, new_package)
+
+            # place packages in various deliveries depending on certain circumstances
+
+            if '84104' in new_package.zip_code and '10:30' not in new_package.deadline:
+                truck_3.package_list.append(new_package)
+
+            if new_package.deadline != 'EOD':
+                if 'Must' in new_package.special_notes or 'None' in new_package.special_notes:
+                    truck_1.package_list.append(new_package)
+
+            if new_package.id == '14':
+                truck_1.package_list.append(new_package)
+
+            if 'Can only be' in new_package.special_notes or 'Delayed' in new_package.special_notes:
+                truck_2.package_list.append(new_package)
+
+            if new_package not in truck_1.package_list and new_package not in truck_2.package_list and new_package not in truck_3.package_list:
+                truck_2.package_list.append(new_package) if len(truck_2.package_list) < len(truck_3.package_list) else truck_3.package_list.append(new_package)
+
