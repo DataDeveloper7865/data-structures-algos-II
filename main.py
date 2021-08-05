1b75ec42d4dd3b8bb4eb1cafe49d9afd7fbab46d
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-05 10:53:07 -0700

    added delivery method call in main

--- main.py
+++ main.py
@@ -15,22 +15,28 @@ def main():
     truck_3 = Truck(3, "11:00:00")
 
     # instantiate hash map to hold values for fast lookup
-    package_holder = HashMap()
+    packages = HashMap()
 
-
-    # 1. read in excel data 
-    # 2. create packages 
+    # 1. read in excel data
+    # 2. create packages
     # 3. store in hash table
     # 4. place packages into trucks
-    create_packages(package_holder, truck_1, truck_2, truck_3)
-
+    create_packages(packages, truck_1, truck_2, truck_3)
 
     #print packages in trucks
     truck_1.show_packages()
     truck_2.show_packages()
     truck_3.show_packages()
 
-    # place packages in trucks
+    # Deliver all the packages
+    # method returns distance traveled for package
+    total_distance_truck_1 = truck_1.deliver_packages()
+    total_distance_truck_2 = truck_2.deliver_packages()
+    total_distance_truck_3 = truck_3.deliver_packages()
+
+    # Add the total distance traveled by each truck
+    total_distance = total_distance_truck_1 + total_distance_truck_2 + total_distance_truck_3
+    print(f"""The total distance traveled for all trucks is {total_distance}""")
 
     # show user interface
     show_user_interface()
