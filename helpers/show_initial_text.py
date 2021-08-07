828f61c796662fbe534ffc79f346bcacb89889c1
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-06 21:39:59 -0700

    added student ID

--- helpers/show_initial_text.py
+++ helpers/show_initial_text.py
@@ -1,6 +1,9 @@
 
-
+# Shows text when the program is started
 def show_initial_text():
     print("Welcome to the WGU Routing application!!")
     print("Written by Stephen Whelan")
-
+    print("Student ID: #000904690")
+    print("\n")
+    print("VROOM VROOM!!")
+    print("The trucks are out for delivery!")
