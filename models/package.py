b771830de146cdb86861ff344bf7751ec331e08e
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-06 21:41:20 -0700

    added repr, comments & deleted unused methods

--- models/package.py
+++ models/package.py
@@ -1,40 +1,36 @@
 
-
 class Package:
 
-    def __init__(self, package_id, address, city, state,
-                    zip_code, deadline, kilos, special_notes,
-                    delivery_time):
+    # contstructor of package class
+    def __init__(self, package_id, address, city, state, zip_code, deadline, kilos, special_notes, delivery_time):
         self.package_id = package_id
         self.address = address
         self.city = city
         self.state = state
         self.zip_code = zip_code
         self.deadline = deadline
-        self.kilos = kilos,
-        self.special_notes = special_notes,
+        self.kilos = kilos
+        self.special_notes = special_notes
         self.delivery_time = delivery_time
 
+    #  repr method to print instantiated packages
     def __repr__(self):
-        return repr(f"""id: {self.package_id} Notes: {self.special_notes} Deadline: {self.deadline}""")
+        tab = '\n'
+        return f"""{tab}
+            package id: {self.package_id} {tab}
+            Delivery Address: {self.address} {tab}
+            City: {self.city} {tab}
+            State: {self.state} {tab}
+            Zip Code: {self.zip_code} {tab}
+            Deadline: {self.deadline} {tab}
+            Kilos: {self.kilos} {tab}
+            Notes: {self.special_notes} {tab}
+            Delivered At: {self.delivery_time} {tab}"""
 
+    # return string of address
     def get_address(self):
         return self.address
 
-    def get_city(self):
-        return self.city
-
-    def get_state(self):
-        return self.state
-
-    def get_zip_code(self):
-        return self.zip_code
-
-    def get_deadline(self):
-        return self.deadline
-
-    def get_kilos(self):
-        return self.kilos
-
-    def get_notes(self):
-        return self.special_notes
+    # set the delivered time of the package property
+    def set_delivered_time(self, time):
+        self.delivery_time = time
