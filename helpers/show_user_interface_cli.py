4113012c20a396dfb42baf1b5eba22bc9cfe6070
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-06 21:40:32 -0700

    added clarifying comments, called methods

--- helpers/show_user_interface_cli.py
+++ helpers/show_user_interface_cli.py
@@ -1,24 +1,34 @@
-from helpers.get_all_package_info import get_all_package_info
 from helpers.get_single_package_info import get_single_package_info
+from helpers.get_all_package_info import get_all_package_info
 
-def show_user_interface():
+# function to show user interface to user
+# takes each truck and references each truck's associated start time
+def show_user_interface(truck_1, truck_2, truck_3, truck_1_start_time, truck_2_start_time, truck_3_start_time
+):
 
-    print("hello world from user interface")
-
+    # grab user input to direct user to different section's of the program
     user_input = input("""
         Please select an option below to begin or type 'quit' to quit:
             - To get info for all packages at a particular time, type 'all_packages'
-            - To get info for a single package at a particular time, type 'package'
+            - To get info for a single package type 'package'
         """)
 
     while user_input != 'quit':
 
+        # if user wants to view all packages
         if user_input == 'all_packages':
 
-            get_all_package_info()
-        
+            # call associated method
+            get_all_package_info(truck_1, truck_2, truck_3, truck_1_start_time, truck_2_start_time, truck_3_start_time)
+            show_user_interface(truck_1, truck_2, truck_3, truck_1_start_time, truck_2_start_time, truck_3_start_time)
+
+        # if user wants to view information on an individual package
         elif user_input == 'package':
 
-            get_single_package_info()
+            # call associated method
+            get_single_package_info(truck_1, truck_2, truck_3)
+            show_user_interface(truck_1, truck_2, truck_3, truck_1_start_time, truck_2_start_time, truck_3_start_time)
 
-
+        elif user_input == 'quit':
+            print("Goodbye!")
+            exit()
