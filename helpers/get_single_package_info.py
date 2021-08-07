91b22d4b4198657af09f50d0f2b7560cb31ebbf4
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-06 21:38:52 -0700

    added single package info methods, called method

--- helpers/get_single_package_info.py
+++ helpers/get_single_package_info.py
@@ -1,13 +1,23 @@
 
+# return the information for a single package
+def get_single_package_info(truck_1, truck_2, truck_3):
 
-def get_single_package_info():
+    # Get the user input on which package to view
+    user_input = input("""
+        Please select an option below to begin or type 'quit' to quit:
+            - please type a package number (1 through 40) to view information for that package:
+        """)
 
-    print("hello world from single package info")
+    # Iterate through each truck list to find package information
+    return_package_info(truck_1, user_input)
+    return_package_info(truck_2, user_input)
+    return_package_info(truck_3, user_input)
+    return
 
-    print("The program will exit now")
-
-
-    # do some stuff here
-
-
-
+# Iterate through truck packages, if it is the packge ID print the package information
+def return_package_info(truck, id):
+    for package in truck.delivered_package_list:
+        if package.package_id == id:
+            print(package)
+            return
+    return
